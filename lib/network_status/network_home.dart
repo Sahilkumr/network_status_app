4a9cc2dc5135fb2902649ae444a3f507c033ca6a
sahilparmar <sahilkumr.dev@gmail.com>
2024-01-30 17:08:33 +0530

    Bloc Listener : Snackbar Added.

--- lib/network_status/network_home.dart
+++ lib/network_status/network_home.dart
@@ -8,39 +8,59 @@ class NetworkHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Connection Status',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          BlocBuilder<NetworkBloc, NetworkState>(
-            builder: (context, state) {
-              if (state is NetworkConnectedState) {
-                return const Text('Connected',
-                    style: TextStyle(
-                        color: Colors.green,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold));
-              } else if (state is NetworkDisconnectedState) {
-                return const Text('Disconnect',
-                    style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold));
-              } else {
-                return const Text('Loading...',
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold));
-              }
-            },
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Connection Status',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            BlocConsumer<NetworkBloc, NetworkState>(
+              listener: (context, state) {
+                if (state is NetworkConnectedState) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      duration: Duration(seconds: 2),
+                      showCloseIcon: true,
+                      content: Text('You are Connnected to Internet'),
+                    ),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      duration: Duration(seconds: 2),
+                      showCloseIcon: true,
+                      content: Text('You are DisConnnected From Internet'),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state is NetworkConnectedState) {
+                  return const Text('Connected',
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold));
+                } else if (state is NetworkDisconnectedState) {
+                  return const Text('Disconnect',
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold));
+                } else {
+                  return const Text('Loading...',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold));
+                }
+              },
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
